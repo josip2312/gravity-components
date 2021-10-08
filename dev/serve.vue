20661a0c9eefe78f1a212b55fe3972b8089a9514
Josip Ivancic <ivancic.josip@nsoft.com>
2021-10-08 11:09:54 +0200

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { GravityComponentsSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  GravityComponentsSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <gravity-components-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as GravityComponentsSample } from './gravity-components-sample.vue';
